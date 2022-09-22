07349bda0b647a4bcd9df79e9ba3addd8699f3d5
Paiynthalir <113221393+Paiynthalir@users.noreply.github.com>
2022-09-22 16:17:59 +1000

    third iteration

--- second research question.R
+++ second research question.R
@@ -14,6 +14,8 @@ library(Hmisc)
 library(lme4)
 library(tidyverse)
 library(readxl)
+library(plyr)
+
 
 Lungcancer_master <- read.csv("~/STDS/STDS_Lung_Cancer_Data_Analysis/Prediction lung cancer.csv")
 
@@ -33,3 +35,28 @@ colSums(is.na(Lungcancer_master))
 
 #Since there are no missing data, no action needed
 
+#To view the column names
+View(Lungcancer_master)
+colnames(Lungcancer_master)
+
+#LUNG_CANCER and GENDER are not numeric fields
+#convert the text field values to numeric values for two columns LUNG_CANCER and GENDER
+Lungcancer_master$GENDER <- revalue(Lungcancer_master$GENDER, c("M"=1))
+Lungcancer_master$GENDER <- revalue(Lungcancer_master$GENDER, c("F"=0))
+
+Lungcancer_master$LUNG_CANCER <- revalue(Lungcancer_master$LUNG_CANCER, c("YES"=1))
+Lungcancer_master$LUNG_CANCER <- revalue(Lungcancer_master$LUNG_CANCER, c("NO"=0))
+
+#View if the GENDER column is now changed to numeric value
+typeof(Lungcancer_master$GENDER)
+typeof(Lungcancer_master$LUNG_CANCER)
+
+#The values are changed to Numeric but the field data type is still character. So we cannot do the correlation matrix
+
+#The data type of GENDER and LUNG_CANCER columns has to be changed to numeric
+
+
+
+
+#Heatmap of the dataset to find what is in the data
+Lungcancer_Correlation <- cor(Lungcancer_master)
